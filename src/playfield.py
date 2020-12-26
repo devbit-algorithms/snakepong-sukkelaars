3936960076d8f7aa0201b8485a4503f4be2961b9
Henry Buysschaert <henry.buysschaert@student.vives.be>
2020-12-26 16:19:38 +0100

    playingfield drawn

--- src/playfield.py
+++ src/playfield.py
@@ -1,5 +1,5 @@
 import pygame
-pygame.init()
+
 
 class Playfield:
     def __init__(self):
@@ -9,10 +9,10 @@ class Playfield:
         WHITE = (255, 255, 255)
         BLACK = (0, 0, 0)
         pygame.draw.rect(screen, BLACK, pygame.Rect(0, 0, 1000, 700))
-        pygame.draw.rect(screen, WHITE, pygame.Rect(0, 0, 1000, 20))
-        pygame.draw.rect(screen, WHITE, pygame.Rect(0, 0, 20, 1000))
-        pygame.draw.rect(screen, WHITE, pygame.Rect(0, 700, 1000, 20))
-        pygame.draw.rect(screen, WHITE, pygame.Rect(1000, 0, 20, 700))
+        pygame.draw.rect(screen, WHITE, pygame.Rect(100, 100, 900, 20)) #top border
+        pygame.draw.rect(screen, WHITE, pygame.Rect(100, 100, 20, 600)) #left border
+        pygame.draw.rect(screen, WHITE, pygame.Rect(100, 700, 920, 20)) #bottom border
+        pygame.draw.rect(screen, WHITE, pygame.Rect(1000, 100, 20, 600)) #right border
         self.__update_screen()
 
     def __update_screen(self):
