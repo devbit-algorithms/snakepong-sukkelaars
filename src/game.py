6abfa09dd0d3705eff0562852f20146f0dafb309
Henry Buysschaert <henry.buysschaert@student.vives.be>
2020-12-26 15:40:53 +0100

    playingfield

--- src/game.py
+++ src/game.py
@@ -1,6 +1,6 @@
 # Imports necessary for running this file
 import pygame
-
+from playfield import Playfield
 # Initialize the pygame environment and its components/modules
 pygame.init()
 surface = pygame.display.set_mode((1200, 800))
@@ -9,3 +9,6 @@ class Game:
     def __init__(self):
         #MAKE THE GAME
         print('MAKE THE GAME')
+        surface.fill((0,0,0))
+        Playfield()
+        pygame.display.flip()
---
+++ src/playfield.py
@@ -0,0 +1,17 @@
+import pygame
+pygame.init()
+
+class Playfield:
+    def __init__(self):
+        screen = pygame.display.set_mode((1200, 800))
+        pygame.display.set_caption("SnakePong")
+        WHITE = (255, 255, 255)
+        BLACK = (0, 0, 0)
+        pygame.draw.rect(screen, BLACK, pygame.Rect(0, 0, 1000, 700))
+        pygame.draw.rect(screen, WHITE, pygame.Rect(0, 0, 1000, 20))
+        pygame.draw.rect(screen, WHITE, pygame.Rect(0, 0, 20, 1000))
+        pygame.draw.rect(screen, WHITE, pygame.Rect(0, 700, 1000, 20))
+        pygame.draw.rect(screen, WHITE, pygame.Rect(1000, 0, 20, 700))
+
+    def 
+
