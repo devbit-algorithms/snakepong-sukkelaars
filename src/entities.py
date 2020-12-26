4c0c563ee12e2211832c1e5de9eda7010aa3f8fc
Henry Buysschaert <henry.buysschaert@student.vives.be>
2020-12-26 17:11:26 +0100

    snake

--- src/entities.py
+++ src/entities.py
@@ -1,1 +1,19 @@
 import pygame
+from playfield import Playfield
+
+screen = pygame.display.set_mode((1200, 800))
+class Snake():
+    def __init__(self):
+        x = 300
+        y = 200
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_UP: y -= 3
+                elif event.key == pygame.K_DOWN: y += 3
+                elif event.key == pygame.K_LEFT: x -= 3
+                elif event.key == pygame.K_RIGHT: x += 3
+        pygame.draw.rect(screen, (0, 128, 255), pygame.Rect(x, y, 10, 10))
+        self.__update_screen()
+
+    def __update_screen(self):
+        pygame.display.flip()
--- src/game.py
+++ src/game.py
@@ -1,18 +1,22 @@
 # Imports necessary for running this file
 import pygame
 from playfield import Playfield
+from entities import Snake
 # Initialize the pygame environment and its components/modules
 pygame.init()
 surface = pygame.display.set_mode((1200, 800))
 
 class Game:
     def __init__(self):
+        clock = pygame.time.Clock()
         #MAKE THE GAME
         print('MAKE THE GAME')
         surface.fill((0,0,0))
 
         Playfield()
+        Snake()
         pygame.display.flip()
+        clock.tick(60)
 
     def keeprunning(self):
         done = False
